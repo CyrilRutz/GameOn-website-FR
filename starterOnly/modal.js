17c9ecf2c6f0d1ed35bdb1bf198818bce7350901
CyrilRutz <71063932+CyrilRutz@users.noreply.github.com>
2021-08-07 11:16:17 +0200

    création du message de validation + changement dans les fonctions

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,14 +1,15 @@
 function editNav() {
-  let x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-} 
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalForm = document.querySelector(".modal-body")
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 //ce que j'ai ajouté 
@@ -23,54 +24,85 @@ const loc1 = modalbg.querySelector ('#location1');
 const utilisation = modalbg.querySelector ('#checkbox1')
 const dateFormat = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
 const numbers = /^[0-9]+$/;
+const mail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
-// launch modal event
+
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
-//ce que j'ai ajouté 
-//close modal event la modale étant a l'origine en display none il faut rétablir l'état initial en cliquant sur close 
+
 modalClose.addEventListener("click", closeModal);
 function closeModal() {
-  modalbg.style.display = "none";
+    modalbg.style.display = "none";
 }
+function closeForm() {
+    modalForm.style.display = "none";
+};
 //validation du formulaire
 modalValidation.addEventListener("click", formValidation);
+
 function formValidation(send){
-    //add error visible attribute
-    formData.forEach(function (currentValue){
-        currentValue.dataset.errorVisible = "false";
-        console.log(formData);
-    });
-        const isFirstValid = firstName.value.length >= 3;
-        const isLastValid = lastName.value.length >= 3;
-        const isEmailValid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(eMail.value);
-        const isBirthdateValid = dateFormat.test(birthDate.value);
-        const isQuantityTournamentValid = numbers.test(quantityTournament.value);
-        const isLocValid = modalbg.querySelector("input[name=location]:checked") !== null;
-        const isUsetermsChecked = utilisation.checked===true;
-        const validationArray = [
-            {input : firstName,data: isFirstValid},
-            {input : lastName,data: isLastValid},
-            {input : eMail,data: isEmailValid},
-            {input : birthDate,data: isBirthdateValid},
-            {input : quantityTournament,data: isQuantityTournamentValid},
-            {input : loc1,data: isLocValid},
-            {input : utilisation , data :isUsetermsChecked}
-        ];
-        if (validationArray.every((currentValue) => currentValue.data === true)) {
-            console.log("merci de votre participation");
-            return;
-        }
-             validationArray.forEach(function (currentValue){
-                 if (currentValue.data === false){
-                     currentValue.input.parentNode.dataset.errorVisible = "true";
-                     console.log(formData);
-                 }
-             });
     send.preventDefault();
 
+    // Effacer les messages d'erreurs
+    clearErrorMessages(formData);
+    const isFirstValid = firstName.value.length >= 3;
+    const isLastValid = lastName.value.length >= 3;
+    const isEmailValid = mail.test(eMail.value);
+    const isBirthdateValid = dateFormat.test(birthDate.value);
+    const isQuantityTournamentValid = numbers.test(quantityTournament.value);
+    const isLocValid = modalbg.querySelector("input[name=location]:checked") !== null;
+    const isUsetermsChecked = utilisation.checked===true;
+    const validationArray = [
+        {input : firstName,data: isFirstValid},
+        {input : lastName,data: isLastValid},
+        {input : eMail,data: isEmailValid},
+        {input : birthDate,data: isBirthdateValid},
+        {input : quantityTournament,data: isQuantityTournamentValid},
+        {input : loc1,data: isLocValid},
+        {input : utilisation , data :isUsetermsChecked}
+    ];
+
+
+    if (validationArray.every((currentValue) => currentValue.data === true)) {
+
+        /*
+         Si tout se passe bien
+            - Réinitialiser les champs du formulaire
+            - Fermer la modal
+         */
+
+        closeForm();
+        clearFormInputs();
+        return;
+    }
+
+    // Appel de la fonction d'affichage des erreurs
+    showErrorMessages(validationArray)
+
+}
+// Fonction responsable de rétirer les messages d'erreur
+function clearErrorMessages(formInputs){
+    formInputs.forEach(function (currentValue){
+        currentValue.dataset.errorVisible = "false";
+    });
+}
+//Fonction responsable d'afficher les erreurs
+function showErrorMessages(validateArray){
+    validateArray.forEach(function (currentValue){
+        if (currentValue.data === false){
+            currentValue.input.parentNode.dataset.errorVisible = "true";
+        }
+    });
+}
+function clearFormInputs(){
+    firstName.value="";
+    lastName.value="";
+    eMail.value="";
+    birthDate.value="";
+    quantityTournament.value="";
+    loc1.value="";
 }
